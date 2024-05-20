e96be81bebb0048bfe3d385b1ec0b13d86352f1f
LucaRicatti <129107816+LucaRicatti@users.noreply.github.com>
2024-05-20 18:15:55 +0200

    Update connect.php

--- php/include/connect.php
+++ php/include/connect.php
@@ -1,7 +1,7 @@
 <?php
-$dbhost = "localhost"; //"44.222.225.194";
-$dbuser = "root"; //"admin";
-$dbpass = ""; //"password";
+$dbhost = "localhost";
+$dbuser = "admin";
+$dbpass = "password";
 $dbname = "AWS";
 $dbport = 3306;
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $dbport);
