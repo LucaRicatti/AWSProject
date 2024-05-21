100ea04d21e9b2507ff34a03d04311225d5bfd20
LucaRicatti <129107816+LucaRicatti@users.noreply.github.com>
2024-05-21 18:54:20 +0200

    fixed everything

--- php/include/connect.php
+++ php/include/connect.php
@@ -1,14 +1,14 @@
 <?php
-$dbhost = "localhost";
-$dbuser = "admin";
-$dbpass = "password";
+$dbhost = "db";
+$dbuser = "root";
+$dbpass = "mariadb";
 $dbname = "AWS";
 $dbport = 3306;
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $dbport);
 
 if($conn -> error){
-	echo "Connection error";   
+	echo "Connection error";
 }else{
-    // echo "Connesso al database con successo";
+	//echo "Connesso al database con successo";
 }
 ?>
