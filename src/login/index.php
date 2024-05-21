cddc38e2010465be3776eab0e015cec95d2f72ea
LucaRicatti <129107816+LucaRicatti@users.noreply.github.com>
2024-05-21 18:52:52 +0200

    fixed everything

--- src/login/index.php
+++ src/login/index.php
@@ -6,12 +6,13 @@ if (isset($_SESSION['logged']) AND $_SESSION['logged'] === true) {
 	return;
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="../../css/loginRegisterStyle.css">
+	<link rel="stylesheet" type="text/css" href="../../css/loginRegisterStyle.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500&display=swap" rel="stylesheet">
